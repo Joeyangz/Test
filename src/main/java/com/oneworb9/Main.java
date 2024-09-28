f7cc45e617e0f370348d123480847b9370846ad0
Oneworb9 <1683625156@qq.com>
2024-09-28 21:07:20 +0800

    Init

---
+++ src/main/java/com/oneworb9/Main.java
@@ -0,0 +1,21 @@
+package com.oneworb9;
+
+/**
+ * 时间:2024/9/28 20:56
+ * 作者:16836   com.oneworb9
+ * 描述:
+ *///TIP 要<b>运行</b>代码，请按 <shortcut actionId="Run"/> 或
+// 点击装订区域中的 <icon src="AllIcons.Actions.Execute"/> 图标。
+public class Main {
+    public static void main(String[] args) {
+        //TIP 当文本光标位于高亮显示的文本处时按 <shortcut actionId="ShowIntentionActions"/>
+        // 查看 IntelliJ IDEA 建议如何修正。
+        System.out.printf("Hello and welcome!");
+
+        for (int i = 1; i <= 5; i++) {
+            //TIP 按 <shortcut actionId="Debug"/> 开始调试代码。我们已经设置了一个 <icon src="AllIcons.Debugger.Db_set_breakpoint"/> 断点
+            // 但您始终可以通过按 <shortcut actionId="ToggleLineBreakpoint"/> 添加更多断点。
+            System.out.println("i = " + i);
+        }
+    }
+}
