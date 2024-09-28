13217095d780b4f23482fa58bcc2ada1bf040636
Oneworb9 <1683625156@qq.com>
2024-09-28 21:16:47 +0800

    test01

---
+++ src/main/java/com/oneworb9/PrintHello.java
@@ -0,0 +1,13 @@
+package com.oneworb9;
+
+/**
+ * 时间:2024/9/28 21:15
+ * 作者:16836   com.oneworb9
+ * 描述:
+ */
+public class PrintHello {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
